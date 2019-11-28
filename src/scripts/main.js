30202fcdb95b9a66e700cc3b397fce82fa45b1e2
Oles Marola <oles_pw@ukr.net>
2019-11-28 14:32:27 +0200

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('hello mate academy!');
+// eslint-disable-next-line no-shadow
+function changeImg(event) {
+  const img = event.target.closest('img');
+
+  if (!img || !list.contains(img)) {
+    return;
+  }
+
+  const largeImg = document.querySelector('.gallery__large-img');
+
+  largeImg.src = img.src.replace('-thumb.jpeg', '.png');
+  largeImg.alt = img.alt;
+  event.preventDefault();
+}
+
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', changeImg);
